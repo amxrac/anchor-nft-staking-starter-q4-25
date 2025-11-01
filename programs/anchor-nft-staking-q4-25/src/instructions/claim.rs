023ab9577259be984db85a5c25103dd7794b88a8
amxrac <108365444+amxrac@users.noreply.github.com>
2025-11-01 11:45:25 +0100

    commit

--- programs/anchor-nft-staking-q4-25/src/instructions/claim.rs
+++ programs/anchor-nft-staking-q4-25/src/instructions/claim.rs
@@ -6,13 +6,64 @@ use anchor_spl::{
 
 use crate::state::{StakeConfig, UserAccount};
 
-// #[derive(Accounts)]
-// pub struct Claim<'info> {
-// //TODO
-// }
+#[derive(Accounts)]
+pub struct Claim<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
 
-// impl<'info> Claim<'info> {
-//     pub fn claim(&mut self) -> Result<()> {
-//     //TODO
-//     }
-// }
+    #[account(
+        mut,
+        seeds = [b"user".as_ref(), user.key().as_ref()],
+        bump = user_account.bump,
+    )]
+    pub user_account: Box<Account<'info, UserAccount>>,
+
+    #[account(
+        init_if_needed,
+        payer = user,
+        associated_token::mint = reward_mint,
+        associated_token::authority = config,
+    )]
+    pub reward_ata: Account<'info, TokenAccount>,
+
+    #[account(
+        seeds = [b"rewards".as_ref(), config.key().as_ref()],
+        bump = config.rewards_bump,
+    )]
+    pub reward_mint: Box<Account<'info, Mint>>,
+
+    #[account(
+        seeds = [b"config".as_ref()],
+        bump = config.bump,
+    )]
+    pub config: Box<Account<'info, StakeConfig>>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Claim<'info> {
+    pub fn claim(&mut self) -> Result<()> {
+        let cpi_program = self.token_program.to_account_info();
+
+        let seeds = &[b"config".as_ref(), &[self.config.bump]];
+        let signer_seeds = &[&seeds[..]];
+
+        let cpi_accounts = MintTo {
+            mint: self.reward_mint.to_account_info(),
+            to: self.reward_ata.to_account_info(),
+            authority: self.config.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        mint_to(
+            cpi_ctx,
+            self.user_account.points as u64 * 10_u64.pow(self.reward_mint.decimals as u32),
+        )?;
+
+        self.user_account.points = 0;
+
+        Ok(())
+    }
+}
--- programs/anchor-nft-staking-q4-25/src/instructions/unstake.rs
+++ programs/anchor-nft-staking-q4-25/src/instructions/unstake.rs
@@ -10,13 +10,92 @@ use crate::{
     state::{StakeAccount, StakeConfig, UserAccount},
 };
 
-// #[derive(Accounts)]
-// pub struct Unstake<'info> {
-// //TODO
-// }
+#[derive(Accounts)]
+pub struct Unstake<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
 
-// impl<'info> Unstake<'info> {
-//     pub fn unstake(&mut self) -> Result<()> {
-// //TODO
-//     }
-// }
+    #[account(
+        mut,
+        constraint = asset.owner == &CORE_PROGRAM_ID @ StakeError::InvalidAsset,
+        constraint = !asset.data_is_empty() @ StakeError::AssetNotInitialized
+    )]
+    /// CHECK: Verified by mpl-core
+    pub asset: UncheckedAccount<'info>,
+
+    #[account(
+        mut,
+        constraint = collection.owner == &CORE_PROGRAM_ID @ StakeError::InvalidCollection,
+        constraint = !collection.data_is_empty() @ StakeError::CollectionNotInitialized
+    )]
+    /// CHECK: Verified by mpl-core
+    pub collection: UncheckedAccount<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"user".as_ref(), user.key().as_ref()],
+        bump = user_account.bump,
+    )]
+    pub user_account: Box<Account<'info, UserAccount>>,
+
+    #[account(
+        seeds = [b"config".as_ref()],
+        bump = config.bump,
+    )]
+    pub config: Box<Account<'info, StakeConfig>>,
+
+    #[account(
+        seeds = [b"stake", config.key().as_ref(), asset.key().as_ref()],
+        bump = stake_account.bump,
+    )]
+    pub stake_account: Account<'info, StakeAccount>,
+
+    #[account(address = CORE_PROGRAM_ID)]
+    /// CHECK: Verified by address constraint
+    pub core_program: UncheckedAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Unstake<'info> {
+    pub fn unstake(&mut self) -> Result<()> {
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"stake",
+            &self.config.key().to_bytes(),
+            &self.asset.key().to_bytes(),
+            &[self.stake_account.bump],
+        ]];
+
+        let time_elapsed = (Clock::get()?.unix_timestamp - self.stake_account.staked_at) as u32;
+
+        require!(
+            time_elapsed > self.config.freeze_period,
+            StakeError::FreezePeriodNotPassed
+        );
+
+        let points_earned = time_elapsed * self.config.points_per_stake as u32;
+        self.user_account.points += points_earned;
+
+        UpdatePluginV1CpiBuilder::new(&self.core_program.to_account_info())
+            .asset(&self.asset.to_account_info())
+            .collection(Some(&self.collection.to_account_info()))
+            .authority(Some(&self.stake_account.to_account_info()))
+            .payer(&self.user.to_account_info())
+            .system_program(&self.system_program.to_account_info())
+            .plugin(Plugin::FreezeDelegate(FreezeDelegate { frozen: false }))
+            .invoke_signed(signer_seeds)?;
+
+        RemovePluginV1CpiBuilder::new(&self.core_program.to_account_info())
+            .asset(&self.asset.to_account_info())
+            .collection(Some(&self.collection.to_account_info()))
+            .authority(None)
+            .payer(&self.user.to_account_info())
+            .system_program(&self.system_program.to_account_info())
+            .plugin_type(PluginType::FreezeDelegate)
+            .invoke()?;
+
+        self.user_account.amount_staked -= 1;
+
+        Ok(())
+    }
+}
